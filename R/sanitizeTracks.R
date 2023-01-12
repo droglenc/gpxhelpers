77be88457e9480ef6ea93097f694d1b02d195a2a
Derek Ogle <DerekOgle51@gmail.com>
2023-01-11 21:39:20 -0600

    Worked on sanitizeTracks()

--- R/sanitizeTracks.R
+++ R/sanitizeTracks.R
@@ -1,16 +1,15 @@
 #' @title Sanitize tracks.
 #' 
-#' @description Sanitize all GPX track files in a directory by removing the  \code{type} and  \code{extension} fields, adding a  \code{desc}ription field, and replacing  \code{time} with dummy times.
+#' @description Sanitize all GPX track files in a directory by removing the \code{type} and  \code{extension} fields, adding a \code{desc}ription field, and replacing \code{time} with dummy times.
+
+#' @param trkinfo A data frame that contains information about each track.
 #' @param pin Path after the working directory that contains the original GPX files.
 #' @param pout Path after the working directory to put the sanitized GPX files.
-#' @param trkinfo A data frame that contains information about each track.
-#' @param moddate A string with a date for which all GPX files in  \code{pin} after this date will be sanitized. Default to today's date.
 #' @param basedate A string with a date that will serve as the base date for the dummy times in the sanitized GPX file. Defaults to "2022-01-01".
-#' @param verbose A logical for whether to show progress or not of files being sanitized.
 #' 
 #' @details NONE YET
 #' 
-#' @return None, used for side effect of writing sanitized GPX tracks to the  \code{pout} directory.
+#' @return None, used for side effect of writing sanitized GPX tracks to the \code{pout} directory.
 #' 
 #' @author Derek H. Ogle
 #' @keywords manip
@@ -19,29 +18,28 @@
 #' ## None yet.
 #' 
 #' @export
-## MAIN function to sanitize all tracks in pin directory
-sanitizeTracks <- function(pin,pout,trkinfo,
-                           moddate=lubridate::today(),basedate="2022-01-01",
-                           verbose=TRUE) {
-  moddate <- as.Date(moddate)
-  ## Find all GPX files in the pin directory within the current wd
-  fns <- list.files(pattern="gpx",path=pin)
-  ## Reduce to only those files modified since the given date
-  finfo <- file.info(file.path(pin,fns))
-  fns <- fns[which(as.Date(finfo$mtime)>moddate)]
-  if (length(fns)==0) cat("\n\nNo new files to sanitize since",
-                          format(moddate,format="%B %e, %Y"),
-                          ".\n\n")
-  else {
-    ## Now sanitize those files
-    for (i in fns) {
+## MAIN function
+sanitizeTracks <- function(trkinfo,pin,pout,basedate="2022-01-01") {
+  # Find all GPX files in the pin directory within the current wd
+  fnins <- list.files(pattern="gpx",path=pin)
+  fninsmtime <- file.info(file.path(pin,fnins))$mtime
+  # Find all GPX files in the pout directory within the current wd
+  fnouts <- list.files(pattern="gpx",path=pout)
+  fnoutsmtime <- max(file.info(file.path(pout,fnouts))$mtime)
+  # Which gpx files in pin have a modtime greater than the max modtime in pout
+  fnneedsan <- fnins[fninsmtime>=fnoutsmtime]
+  if (length(fnneedsan)==0) {
+    cli::cli_alert_warning("No tracks have been modified since {as.character(fnoutsmtime)}. There are no files in {file.path(getwd(),pin)} to sanitize.")
+  }
+  else { ## Now sanitize those files
+    for (i in fnneedsan) {
       trk <- tools::file_path_sans_ext(i)
       tmp <- trkinfo[trkinfo$trackID==trk,]
       if (nrow(tmp)<1)
-        warning(trk,"not found in info file; thus not sanitized!")
+        cli::cli_alert_warning("{trk} not found in info file; thus not sanitized!")
       else {
         desc <- iMakeDescription(tmp$Primary,tmp$From,tmp$To)
-        if (verbose) cat("Sanitizing:",trk,"-",desc,"\n")
+        cli::cli_alert_info("Sanitizing: {trk}-{desc}")
         iSanitizeTrack(f=i,pin=pin,pout=pout,desc=desc,basedate=basedate)
       }
     }
